f2a7317658169bb7f92dc44c3cb96608476b5dea
nayan jha <121750579+jhaNayan2509@users.noreply.github.com>
2025-05-24 15:52:39 +0530

    Updated [chatId]/page.tsx for Fixing Production Issue

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,7 +1,17 @@
+// import ChatWindow from "@/components/ChatWindow";
+
+// export default function ChatIdPage({ params }: { params: { chatId: string } }) {
+//   // No await needed here!
+//   const { chatId } = params;
+//   return <ChatWindow chatId={chatId} />;
+// }
 import ChatWindow from "@/components/ChatWindow";
 
-export default function ChatIdPage({ params }: { params: { chatId: string } }) {
-  // No await needed here!
-  const { chatId } = params;
+export default async function ChatIdPage({
+  params,
+}: {
+  params: Promise<{ chatId: string }>;
+}) {
+  const { chatId } = await params;
   return <ChatWindow chatId={chatId} />;
 }
